36091b4b8ffa1f48ef87fa66b0d6156bcfeea78e
anandsri122201-hue <anand.sri122201@gmail.com>
2026-05-23 22:26:43 +0800

    Added player movement and shooting

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class Entity : MonoBehaviour
 {
     protected Animator anim;
     protected Rigidbody2D rb;
+    protected SpriteRenderer sr;
+
+    [Header("Health")]
+    [SerializeField] private int maxHealth = 10;
+    private int currentHealth;
 
     protected virtual void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        currentHealth = maxHealth;
     }
 
     protected virtual void Update()
@@ -31,5 +39,28 @@ public class Entity : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         else if (rb.linearVelocity.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    public virtual void TakeDamage()
+    {
+        currentHealth--;
+        Debug.Log(gameObject.name + " took damage! HP: " + currentHealth);
+        StartCoroutine(DamageFlash());
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    private IEnumerator DamageFlash()
+    {
+        sr.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sr.color = Color.white;
+    }
+
+    protected virtual void Die()
+    {
+        Debug.Log(gameObject.name + " died!");
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,34 @@ public class Player : Entity
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float crouchSpeed = 2.5f;
 
     [Header("Ground Check")]
     [SerializeField] private float groundCheckDistance = 1.4f;
     [SerializeField] private LayerMask whatIsGround;
 
+    [Header("Shooting")]
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
     private float xInput;
     private bool isGrounded;
+    private bool isCrouching;
+    private int facingDirection = 1;
 
     protected override void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
         CheckGround();
+        HandleCrouch();
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             rb.linearVelocity = new Vector2(
                 rb.linearVelocity.x, jumpForce
             );
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Shoot();
 
         base.Update();
     }
@@ -36,11 +47,37 @@ public class Player : Entity
         );
     }
 
+    private void HandleCrouch()
+    {
+        isCrouching = Input.GetKey(KeyCode.S)
+                   || Input.GetKey(KeyCode.DownArrow);
+    }
+
+    private void Shoot()
+    {
+        GameObject newBullet = Instantiate(
+            bulletPrefab,
+            firePoint.position,
+            Quaternion.identity
+        );
+        newBullet.GetComponent<Bullet>()
+                 .SetDirection(facingDirection);
+    }
+
     protected override void HandleMovement()
     {
-        rb.linearVelocity = new Vector2(
-            xInput * moveSpeed,
-            rb.linearVelocity.y
-        );
+        if (xInput != 0)
+            facingDirection = xInput > 0 ? 1 : -1;
+
+        if (isCrouching)
+            rb.linearVelocity = new Vector2(
+                xInput * crouchSpeed,
+                rb.linearVelocity.y
+            );
+        else
+            rb.linearVelocity = new Vector2(
+                xInput * moveSpeed,
+                rb.linearVelocity.y
+            );
     }
 }
