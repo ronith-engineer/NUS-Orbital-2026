352d4507d0731c3363c1d2c170b7625b865fa26e
Ronith Kartikeyan <ronithkar14831@gmail.com>
2026-05-25 00:55:36 +0530

    Merging changes from branch srinivasan to branch ronith (2)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed = 15f;
+    private int direction;
+
+    public void SetDirection(int dir)
+    {
+        direction = dir;
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(
+            bulletSpeed * direction * Time.deltaTime,
+            0,
+            0
+        );
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Entity>()?.TakeDamage();
+            Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("Ground"))
+            Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
