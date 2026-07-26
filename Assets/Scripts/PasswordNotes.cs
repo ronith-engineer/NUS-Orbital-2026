6fca66a58d7eec2643b135ed166373f038005e75
Ronith Kartikeyan <ronithkar14831@gmail.com>
2026-07-26 17:46:52 +0530

    Add slow motion when notes are open

--- Assets/Scripts/PasswordNotes.cs
+++ Assets/Scripts/PasswordNotes.cs
@@ -6,21 +6,26 @@ public class PasswordNotes : MonoBehaviour, ICloseableUI
     [SerializeField] private GameObject noteCanvas;
     private bool isPlayerNearby = false;
 
+    [Header("Slow Motion")]
+    [SerializeField] private float slowMotionScale = 0.1f;
+    [SerializeField] private bool useSlowMotion = true;
+
     void Update()
     {
         if (isPlayerNearby)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                noteCanvas.SetActive(true);
-                MenuManager.Instance.RegisterOpenUI(this);
-                Player.Instance.EnableMovementAndJump(false);
+                OpenUI();
             }
         }
         else
         {
             if (noteCanvas.activeSelf)
+            {
                 CloseUI();
+            }
+                
         }
 
     }
@@ -55,6 +60,31 @@ public class PasswordNotes : MonoBehaviour, ICloseableUI
         noteCanvas.SetActive(false);
         Player.Instance.EnableMovementAndJump(true);
         MenuManager.Instance.UnregisterOpenUI(this);
+        SetSlowMotion(false);
+    }
+
+    public void OpenUI()
+    {
+        noteCanvas.SetActive(true);
+        Player.Instance.EnableMovementAndJump(false);
+        MenuManager.Instance.RegisterOpenUI(this);
+        SetSlowMotion(true);
+    }
+
+    private void SetSlowMotion(bool slow)
+    {
+        if (!useSlowMotion) return;
+
+        if (slow)
+        {
+            Time.timeScale = slowMotionScale;
+            Time.fixedDeltaTime = 0.02f * slowMotionScale;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = 0.02f;
+        }
     }
 
 
